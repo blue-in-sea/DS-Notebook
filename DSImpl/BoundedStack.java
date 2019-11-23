80ecc8eb510b91e69eff7cc233d1e63891c9ed0a
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-23 16:53:23 -0600

    Update BoundedStack.java

--- DSImpl/BoundedStack.java
+++ DSImpl/BoundedStack.java
@@ -1,46 +1,46 @@
 public class BoundedStack {
     private int head;
     private int[] array;
-    
+
     public BoundedStack(int cap) {
         array = new int[cap];
-        head = -1;  
+        head = -1;
     }
-    
+
     public boolean push(int ele) {
         if (head == array.length -1) {
             return false;
         }
         array[++head] = ele;
-        return true
+        return true;
     }
-    
+
     public Integer pop() {
         return head == -1 ? 0 : array[head--];
     }
-    
+
     public Integer top() {
         return head == -1 ? 0 : array[head];
     }
-    
+
     public boolean isEmpty() {
         return head == -1;
     }
-    
-    
+
+
     public static void main(String[] args) {
-        BoundedStack stackArr = new BoundedStack(100);
+        BoundedStack MyArrStack = new BoundedStack(100);
 
-        stackArr.push(4);
-        stackArr.push(5);
-        stackArr.push(6);
+        MyArrStack.push(4);
+        MyArrStack.push(5);
+        MyArrStack.push(6);
 
-        while (!stackArr.isEmpty()) {
-            System.out.println(stackArr.top());
-            System.out.println(stackArr.pop());
+        while (!MyArrStack.isEmpty()) {
+            System.out.println(MyArrStack.top());
+            System.out.println(MyArrStack.pop());
         }
 
-        System.out.println(stackArr.top());
-        System.out.println(stackArr.pop());
+        System.out.println(MyArrStack.top());
+        System.out.println(MyArrStack.pop());
     }
 }
