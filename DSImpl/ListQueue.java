4956c5bd7500ac4758c3605147b18cdfcdc0df6a
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-23 16:39:59 -0600

    Update ListQueue.java

--- DSImpl/ListQueue.java
+++ DSImpl/ListQueue.java
@@ -1,3 +1,5 @@
+package DataStr;
+
 /**
  * public class ListNode {
  *   public int value;
@@ -8,59 +10,64 @@
  *   }
  * }
  */
+
 public class ListQueue {
-  private ListNode head;
-  private ListNode tail;
-  
-  public ListQueue() {
-    head = tail = null;
-  }
-  
-  public void offer(Integer ele) {
-    if (head == null) {
-      head = new ListNode(ele);
-      tail = head;
-    } else {
-      tail.next = new ListNode(ele);
-      tail = tail.next;
-    }
-  }
-  
-  public Integer peek() {
-    if (head == null) {
-      return null;
-    }
-    return head.value;
-  }
-  
-  public Integer poll() {
-    if (head == null) {
-      return null;
-    }
-    ListNode node = new ListNode(head.value);
-    if (head.next == null) {
-      tail = null;
-    }
-    return head.value;
-  }
-  
-  public boolean isEmpty() {
-    return head == null && tail == null;
-  }
-  
-  public static void main(String[] args) {
-    BoundedQueue queueLL = new BoundedQueue(100);
-    
-    queueLL.offer(4);
-    queueLL.offer(5);
-    queueLL.offer(6);
+    private ListNode head;
+    private ListNode tail;
 
-    while (!queueLL.isEmpty()) {
-      System.out.println(queueLL.peek());
-      System.out.println(queueLL.poll());
+    public ListQueue() {
+        this.head = this.tail = null;
     }
 
-      System.out.println(queueLL.peek());
-      System.out.println(queueLL.poll());
-  }
+    public void offer(Integer ele) {
+        if (head == null) {
+            head = new ListNode(ele);
+            tail = head;
+        } else {
+            tail.next = new ListNode(ele);
+            tail = tail.next;
+        }
+    }
+
+    public Integer poll() {
+        if (head == null) {
+            return null;
+        }
+        ListNode node = head;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        node.next = null;
+        return node.value;
+    }
+
+    public Integer peek() {
+        if (head == null) {
+            return null;
+        }
+        return head.value;
+    }
+
+
+    public boolean isEmpty() {
+        return head == null && tail == null;
+    }
+
+    public static void main(String[] args) {
+        ListQueue queueLL = new ListQueue();
+
+        queueLL.offer(4);
+        queueLL.offer(5);
+        queueLL.offer(6);
+
+        while (!queueLL.isEmpty()) {
+            System.out.println(queueLL.peek());
+            System.out.println(queueLL.poll());
+        }
+
+        System.out.println(queueLL.peek());
+        System.out.println(queueLL.poll());
+    }
 }
+
