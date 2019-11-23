2ed4f941252e2f1504efd8e6b10b6d419fc07052
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-22 23:36:39 -0600

    Update MinStack.java

--- MinStack.java
+++ MinStack.java
@@ -1,3 +1,4 @@
+// Method 1: Synchronously push two stacks
 public class MinStack {
     Stack<Integer> stack;
     Stack<Integer> minStack;
@@ -25,6 +26,48 @@ public class MinStack {
      */
     public int pop() {
         minStack.pop();
+        return stack.pop();
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int min() {
+        return minStack.peek();
+    }
+}
+
+// Method 2: more space saved, while worst case space complexity doesn't change
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+    
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    /*
+     * @param number: An integer
+     * @return: nothing
+     */
+    public void push(int number) {
+        stack.push(number);
+        if (minStack.isEmpty()) {
+            minStack.push(number);
+        } else if (minStack.peek() >= number) {
+            // 这里考虑的相等的情况也会继续push
+            minStack.push(number);
+        }
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int pop() {
+        if (stack.peek().equals(minStack.peek())){
+            minStack.pop();
+        }
         return stack.pop();
     }
 
