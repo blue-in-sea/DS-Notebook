2d8683fa6b6ec12a42b1deb4704e061ca354fa57
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2019-11-23 17:02:06 -0600

    Create ListStack.java

---
+++ DSImpl/ListStack.java
@@ -0,0 +1,52 @@
+package DataStr;
+
+public class ListStack {
+    private ListNode head;
+
+    public ListStack() {
+        this.head = null;
+    }
+
+    public void push(Integer ele) {
+        ListNode node = new ListNode(ele);
+        node.next = head;
+        head = node;
+    }
+
+    public Integer pop() {
+        if (head == null) {
+            return null;
+        }
+        ListNode node = head;
+        head = head.next;
+        node.next = null;
+        return node.value;
+    }
+
+    public Integer top() {
+        if (head == null) {
+            return null;
+        }
+        return head.value;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public static void main(String[] args) {
+        ListStack stackLL = new ListStack();
+
+        stackLL.push(1);
+        stackLL.push(2);
+        stackLL.push(3);
+
+        while (!stackLL.isEmpty()) {
+            System.out.println(stackLL.top());
+            System.out.println(stackLL.pop());
+        }
+
+        System.out.println(stackLL.top());
+        System.out.println(stackLL.pop());
+    }
+}
